42d5307ac4f8c0d20ff42edee2b171c51d1139f1
Pop2323 <omar4500455@gmail.com>
2025-02-13 18:38:44 +0200

    Understanding_Array_Attributes

---
+++ Day 12/Understanding_Array_Attributes.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# Shape, Size, and Data Type:
+matrix = np.array([[1, 2, 3], [4, 5, 6]])
+
+print("Matrix:\n", matrix)
+print("Shape:", matrix.shape)  # (rows, columns)
+print("Size:", matrix.size)    # Total number of elements
+print("Data Type:", matrix.dtype)
